77ff4aa39643acda608deec1d129827127defffd
Rishab Kumar <rishabkumar7@gmail.com>
2021-02-22 12:07:47 -0500

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        trackingId: "G-KM4CDK1X34",
         head: true,
       }
     },
